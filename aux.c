ca5d5dbc96c17d78256a9fccb8a85b07c7b29d89
Petter Holme <pttrhlm@gmail.com>
2017-08-30 15:38:53 +0900

    Update aux.c
    This update increases the performance usually 10-100 times (but more for symmetric graphs) by testing for isomorphic configurations (thus pruning the tree of SIR configurations). It uses the IGraph http://igraph.org/ library (in addition to the FLINT library http://libflint.org/).

--- aux.c
+++ aux.c
@@ -1,19 +1,22 @@
 #include "poly.h"
 
-extern NODE n[N];
 extern GLOBAL g;
+extern NODE n[MAXN];
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// calculated the outbreak size if no more nodes get infected
 
 int obsize () {
-	int i, si = 0;
+	int i, s = 0;
 
-	for (i = 0; i < g.n; i++) if ((n[i].state == I) || (n[i].state == R)) si++;
+	for (i = 0; i < g.n; i++)
+		if ((IS_INFECTIOUS(i)) || (IS_RECOVERED(i))) s++;
 
-	return si;
+	return s;
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// building the network
 
 void add_edge (int me, int you) {
 
@@ -25,6 +28,7 @@ void add_edge (int me, int you) {
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// changing the FLINT style polynomial pretty print to a SymPy friendly format
 
 void strc (char *sin, char *sout) {
 	int l;
@@ -42,12 +46,13 @@ void strc (char *sin, char *sout) {
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// eliminates common factors from polys p and q, thus simplifying p/q
 
 void simplify (fmpz_poly_t *p, fmpz_poly_t *q) {
 
-	fmpz_poly_gcd(g.a, *p, *q);
-	fmpz_poly_div(*p, *p, g.a);
-	fmpz_poly_div(*q, *q, g.a);
+	fmpz_poly_gcd(g.s, *p, *q);
+	fmpz_poly_div(*p, *p, g.s);
+	fmpz_poly_div(*q, *q, g.s);
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
