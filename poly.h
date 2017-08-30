aedcc8e77c8c6f1f2db9fed8be2f87b7ef34a7e1
Petter Holme <pttrhlm@gmail.com>
2017-08-30 15:41:18 +0900

    Update poly.h
    This update increases the performance usually 10-100 times (but more for symmetric graphs) by testing for isomorphic configurations (thus pruning the tree of SIR configurations). It uses the IGraph http://igraph.org/ library (in addition to the FLINT library http://libflint.org/).

--- poly.h
+++ poly.h
@@ -3,25 +3,36 @@
 #include <string.h>
 #include <fmpz_poly.h>
 
-#define N 8 // max number of nodes 
-#define S 0
-#define I 1
-#define R 2
-#define V 3
-#define SENTI 4
-#define OSIZE 10000
+#define SUSCEPTIBLE 0 // flags for the node states
+#define SENTINEL 1
+#define INFECTIOUS 2
+#define RECOVERED 3
+#define VACCINATED 4
+#define MAXN 8 // maximal number of nodes
+#define OSIZE 10000 // for printing output
+
+#define TRUE 1
+#define FALSE 0
+
+#define IS_SUSCEPTIBLE(x) (n[(x)].state < INFECTIOUS) // sentinels are also susceptible
+#define IS_INFECTIOUS(x) (n[(x)].state == INFECTIOUS)
+#define IS_RECOVERED(x) (n[(x)].state == RECOVERED)
+#define IS_SENTINEL(x) (n[(x)].state == SENTINEL)
+#define NOT_VACCINATED(x) (n[(x)].state != VACCINATED)
+#define NOT_SENTINEL(x) (n[(x)].state != SENTINEL)
 
 typedef struct {
-	int deg, nb[N], state;
+	int deg, nb[MAXN], state, id;
 } NODE;
 
 typedef struct {
-	int n, nl;
-	char s[OSIZE];
-	fmpz_poly_t onum, oden, a, b;
+	int n, nn, nl, infectables[MAXN], recoverables[MAXN], check[MAXN];
+	fmpz_poly_t onum, oden, a, b, s;
 } GLOBAL;
 
-void simplify (fmpz_poly_t *, fmpz_poly_t *);
-int obsize ();
-void add_edge (int, int);
-void strc (char *, char *);
+void simplify (fmpz_poly_t *, fmpz_poly_t *); // in poly.c
+int obsize (); // in poly.c
+void add_edge (int, int); // in poly.c
+void strc (char *, char *); // in poly.c
+void stepdown (fmpz_poly_t, fmpz_poly_t); // in stepdown.c
+void get_inf_rec (int *, int *, int *, int *, int *, int *); // in get_inf_rec.c
